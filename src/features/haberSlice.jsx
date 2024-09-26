8f5e3df7abb0da12ab8638cce4c5c872ebd6bc37
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-09-26 22:47:10 +0300

    apiden veriler çekilde news sayfasıyla paylaşılacak

--- src/features/haberSlice.jsx
+++ src/features/haberSlice.jsx
@@ -1,4 +1,12 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const getData = createAsyncThunk("haberSlice/getData", async () => {
+  const response = await axios(
+    "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=81a4163ea7eb4bccb489151972100adb"
+  );
+  return response.data.articles;
+});
 
 export const haberSlice = createSlice({
   name: "haberSlice",
@@ -10,7 +18,16 @@ export const haberSlice = createSlice({
     clearHaber: () => {},
   },
 
-  extraReducers: () => {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getData.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getData.fulfilled, (state, action) => {
+        state.haberler = action.payload;
+        state.loading = false;
+      });
+  },
 });
 export const { clearHaber } = haberSlice.actions;
 export default haberSlice.reducer;
